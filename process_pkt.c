c0f1d7039c5710f03c083cc323b86090dd75f0bd
Nauman Badar <nauman.gwt@gmail.com>
2011-05-27 16:45:09 +0200

    removed headers. Now getting IP packet

--- process_pkt.c
+++ process_pkt.c
@@ -1,39 +1,25 @@
-//#include "bouncer.h"
+#define SIZE_ETHERNET 14
 
+void process_pkt(u_char *args, const struct pcap_pkthdr *header, const u_char *packet){
 
-void process_pkt(u_char *args, const struct pcap_pkthdr *header,
-	    const u_char *packet){
+		const struct ip *ip_header; /* The IP header */
 
-//	printf("Packet captured. \n");
-	/* Define pointers for packet's attributes */
-		const struct sniff_ethernet *ethernet; /* The ethernet header */
-		const struct sniff_ip *ip; /* The IP header */
-		const struct sniff_tcp *tcp; /* The TCP header */
-		const char *payload; /* Packet payload */
 
 		u_int size_ip;
-		u_int size_tcp;
 
-			ethernet = (struct sniff_ethernet*)(packet);
-			ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
-			size_ip = IP_HL(ip)*4;
+			ip_header = (struct ip*)(packet + SIZE_ETHERNET);
+			size_ip = ip_header->ip_hl*4;
 			if (size_ip < 20) {
 				printf("   * Invalid IP header length: %u bytes\n", size_ip);
 				return;
 			}
-			tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
-			size_tcp = TH_OFF(tcp)*4;
-			if (size_tcp < 20) {
-				printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
-				return;
-			}
-			payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
 	
 	/* Check IP header*/
-		printf("Packet received from %s\n",inet_ntoa(ip->ip_src));
+		printf("Packet received from %s\n",inet_ntoa(ip_header->ip_src));
 	/* Check type of packet and process*/
-
 	
+
+
 	/* Check ICMP header*/
 	/* Check TCP header*/
 	/* Check FTP header*/
