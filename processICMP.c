8175a9268a7820ad8b645d0a58b5fab0ed683403
Nauman Badar <nauman.gwt@gmail.com>
2011-05-28 17:29:52 +0200

    pingWorkingOK

--- processICMP.c
+++ processICMP.c
@@ -31,34 +31,6 @@ void processICMP(struct icmp *icmp_header, struct ip *ip_header) {
 
 		sendIp(ip_header);
 
-		//	printf("Ping new src from %s ", inet_ntoa(ip_header->ip_src));
-		//	printf("to %s ID:%d icm_state_size %d\n", inet_ntoa(ip_header->ip_dst), icmp_header->icmp_hun.ih_idseq.icd_id, icmpStateList->size);
-
-
-//		int fd = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
-//		int headerIncluided = 1;
-//		if (setsockopt(fd, IPPROTO_IP, IP_HDRINCL, &headerIncluided, sizeof(headerIncluided))
-//				== 0) {
-//			//			printf("OPTS set\n");
-//		}
-//
-//		struct sockaddr_in serverAddress;
-//		serverAddress.sin_family = AF_INET;
-////		serverAddress.sin_addr.s_addr = inet_addr(serverIP);
-//		serverAddress.sin_addr.s_addr = ip_header->ip_dst.s_addr;
-//
-//		printf("Ping new src from %s ", inet_ntoa(ip_header->ip_src));
-//		printf("to %s ID:%d icm_state_size %d\n", inet_ntoa(ip_header->ip_dst), icmp_header->icmp_hun.ih_idseq.icd_id, icmpStateList->size);
-//		int errno;
-//
-//		if (sendto(fd, (char*) ip_header, ntohs(ip_header->ip_len), 0, (struct sockaddr*) &serverAddress, sizeof(struct sockaddr))
-//				== -1) {
-//			printf("sending error %d %d\n", errno, ntohs(ip_header->ip_len));
-//			perror("error");
-//		}
-//
-//		close(fd);
-
 	}
 
 	if (icmp_header->icmp_type == ICMP_ECHOREPLY) {
@@ -66,65 +38,38 @@ void processICMP(struct icmp *icmp_header, struct ip *ip_header) {
 		printf("ECHOREPLY received from %s ", inet_ntoa(ip_header->ip_src));
 		printf("to %s ID:%d icm_state_size %d\n\n\n", inet_ntoa(ip_header->ip_dst), icmp_header->icmp_hun.ih_idseq.icd_id, icmpStateList->size);
 
+		struct icmp_state state;
+		state.idNumber = icmp_header->icmp_hun.ih_idseq.icd_id;
+		state.ip_src = ip_header->ip_src;
+		struct icmp_state * savedState;
+		if ((savedState
+				= fetchNode(icmpStateList, &state, icmp_stateInList)->data)
+				==NULL) {
+			printf("Saved State Not Found\n");
+			return;
+		}
 
-				struct icmp_state state;
-				state.idNumber = icmp_header->icmp_hun.ih_idseq.icd_id;
-				state.ip_src = ip_header->ip_src;
-				struct icmp_state * savedState;
-				if((savedState=fetchNode(icmpStateList, &state, icmp_stateInList)->data)==NULL){
-					printf("Saved State Not Found\n");
-					return;
-				}
+		ip_header->ip_src.s_addr = inet_addr(listenIP);
+		ip_header->ip_dst.s_addr = savedState->ip_src.s_addr;
 
-						ip_header->ip_src.s_addr = inet_addr(listenIP);
-						ip_header->ip_dst.s_addr = savedState->ip_src.s_addr;
-
-
-						sendIp(ip_header);
-
-//						int fd = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
-//						int headerIncluided = 1;
-//						if (setsockopt(fd, IPPROTO_IP, IP_HDRINCL, &headerIncluided, sizeof(headerIncluided))
-//								== 0) {
-//							//			printf("OPTS set\n");
-//						}
-//
-//						struct sockaddr_in serverAddress;
-//						serverAddress.sin_family = AF_INET;
-//				//		serverAddress.sin_addr.s_addr = inet_addr(serverIP);
-//						serverAddress.sin_addr.s_addr = ip_header->ip_dst.s_addr;
-//
-//						printf("*******ECHO REPLY new src from %s ", inet_ntoa(ip_header->ip_src));
-//						printf("to %s ID:%d icm_state_size %d\n", inet_ntoa(ip_header->ip_dst), icmp_header->icmp_hun.ih_idseq.icd_id, icmpStateList->size);
-//						int errno;
-//
-//						if (sendto(fd, (char*) ip_header, ntohs(ip_header->ip_len), 0, (struct sockaddr*) &serverAddress, sizeof(struct sockaddr))
-//								== -1) {
-//							printf("sending error %d %d\n", errno, ntohs(ip_header->ip_len));
-//							perror("error");
-//						}
-//
-//						close(fd);
-
+		sendIp(ip_header);
 
 	}
 }
 
-void sendIp(struct ip *ip_header){
+void sendIp(struct ip *ip_header) {
 
 	int fd = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
 	int headerIncluided = 1;
 	if (setsockopt(fd, IPPROTO_IP, IP_HDRINCL, &headerIncluided, sizeof(headerIncluided))
-			== 0) {
-		//			printf("OPTS set\n");
+			!= 0) {
+					printf("Error in OPTS set\n");
 	}
 
 	struct sockaddr_in serverAddress;
 	serverAddress.sin_family = AF_INET;
 	serverAddress.sin_addr.s_addr = ip_header->ip_dst.s_addr;
 
-//	printf("Ping new src from %s ", inet_ntoa(ip_header->ip_src));
-//	printf("to %s ID:%d icm_state_size %d\n", inet_ntoa(ip_header->ip_dst), icmp_header->icmp_hun.ih_idseq.icd_id, icmpStateList->size);
 	int errno;
 
 	if (sendto(fd, (char*) ip_header, ntohs(ip_header->ip_len), 0, (struct sockaddr*) &serverAddress, sizeof(struct sockaddr))
