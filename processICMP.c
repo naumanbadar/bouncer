0de9b12f4f897bfed933d6ae0b9347667df39ddd
Nauman Badar <nauman.gwt@gmail.com>
2011-05-28 02:34:06 +0200

    packet is being sent with changed src and dst as seen on wireshark

--- processICMP.c
+++ processICMP.c
@@ -1,6 +1,7 @@
 #include "states.h"
 #include "linkedList.h"
 #include "bouncer.h"
+#include <unistd.h>
 
 void processICMP(struct icmp *icmp_header, struct ip *ip_header) {
 
@@ -28,14 +29,30 @@ void processICMP(struct icmp *icmp_header, struct ip *ip_header) {
 
 		int fd = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
 		int headerIncluided = 1;
-		setsockopt(fd, IPPROTO_IP, IP_HDRINCL, &headerIncluided, sizeof(headerIncluided));
+		if (setsockopt(fd, IPPROTO_IP, IP_HDRINCL, &headerIncluided, sizeof(headerIncluided))
+				== 0) {
+			printf("OPTS set\n");
+		}
+
 		struct sockaddr_in serverAddress;
+		serverAddress.sin_family = AF_INET;
 		serverAddress.sin_addr.s_addr = inet_addr(serverIP);
 
 		printf("Ping new src from %s ", inet_ntoa(ip_header->ip_src));
 		printf("to %s ID:%d icm_state_size %d\n", inet_ntoa(ip_header->ip_dst), icmp_header->icmp_hun.ih_idseq.icd_id, icmpStateList->size);
+		int errno;
 
-		//		sendto(fd, (char*) ip_header, ip_header->ip_len, 0, (struct sockaddr*) &serverAddress, sizeof(struct sockaddr));
+		if (sendto(fd, (char*) ip_header, ntohs(ip_header->ip_len), 0, (struct sockaddr*) &serverAddress, sizeof(struct sockaddr))
+				== -1) {
+			printf("sending error %d %d\n", errno, ntohs(ip_header->ip_len));
+			perror("error");
+		}
+
+		close(fd);
+
+	}
+
+	if(icmp_header->icmp_type == ICMP_ECHOREPLY){
 
 	}
 }
