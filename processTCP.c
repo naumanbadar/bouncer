82638caf40cb873aa5ba285219b9a471435c7978
Nauman Badar <nauman.gwt@gmail.com>
2011-05-30 15:24:18 +0200

    see if packet is correctly sent to client

--- processTCP.c
+++ processTCP.c
@@ -37,7 +37,7 @@ void processTCP(struct tcphdr *tcp_header, struct ip *ip_header) {
 				savedState = (struct tcp_state *) fetchedNode->data;
 
 
-			tcp_header->th_sport = htons(listenPort);
+			tcp_header->th_sport = htons(atoi(listenPort));
 				tcp_header->th_dport = htons(savedState->senderSourcePort);
 			tcp_header->th_sum = 0;
 			ip_header->ip_src.s_addr = inet_addr(listenIP);
