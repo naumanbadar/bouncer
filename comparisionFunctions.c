b82dc09e0dc4354d00b262bd47aaf4bcfaf5a91f
Nauman Badar <nauman.gwt@gmail.com>
2011-05-30 14:47:27 +0200

    check the port number of server

--- comparisionFunctions.c
+++ comparisionFunctions.c
@@ -7,8 +7,7 @@ int icmp_stateInList(void* data, Node node) {
 
 	if (associationInList->idNumber == associationNew->idNumber) {
 		return 1;
-	}
-	else
+	} else
 		return 0;
 }
 
@@ -16,24 +15,25 @@ int tcp_stateInList_wrt_sourceIpAndSourcePortAndDestinationPort(void* data, Node
 	struct tcp_state * associationInList = (struct tcp_state *) node->data;
 	struct tcp_state * associationNew = (struct tcp_state *) data;
 
-	if (associationInList->senderSourcePort == associationNew->senderSourcePort &&associationInList->senderDestinationPort == associationNew->senderDestinationPort&& associationInList->senderSourceIp.s_addr==associationNew->senderSourceIp.s_addr) {
+	if (associationInList->senderSourcePort == associationNew->senderSourcePort
+			&& associationInList->senderDestinationPort
+					== associationNew->senderDestinationPort
+			&& associationInList->senderSourceIp.s_addr
+					== associationNew->senderSourceIp.s_addr) {
 		return 1;
-	}
-	else
+	} else
 		return 0;
 }
 int tcp_stateInList_wrt_bouncerPortAndIp(void* data, Node node) {
 	struct tcp_state * associationInList = (struct tcp_state *) node->data;
 	struct tcp_state * associationNew = (struct tcp_state *) data;
 
-//	if (associationNew->senderDestinationPort == associationInList->bouncerSourcePort&&associationNew->senderSourceIp.s_addr==associationInList->senderDestinationIp.s_addr) {
-	if (associationNew->senderDestinationPort == associationInList->bouncerSourcePort) {
-		 printf("FROM COMPARISION FUNCTION %s ",inet_ntoa(associationNew->senderSourceIp));
-		printf("%s\n",inet_ntoa(associationInList->senderDestinationIp));
+		if (associationNew->senderDestinationPort == associationInList->bouncerSourcePort&&associationNew->senderSourceIp.s_addr==inet_addr(serverIP)) {
+//	if (associationNew->senderDestinationPort== associationInList->bouncerSourcePort) {
+		//		 printf("FROM COMPARISION FUNCTION %s ",inet_ntoa(associationNew->senderSourceIp));
+		//		printf("%s\n",inet_ntoa(associationInList->senderDestinationIp));
 		return 1;
-	}
-	else
+	} else
 		return 0;
 }
-
 
